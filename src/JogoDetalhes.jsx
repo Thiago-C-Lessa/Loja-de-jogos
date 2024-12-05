6d00ea0d19d24637c29776aebd67425992ff87ad
Caio Leonardo <93164335+Swotty00@users.noreply.github.com>
2024-12-04 22:09:15 -0300

    JogoDetalhes.jsx

--- src/JogoDetalhes.jsx
+++ src/JogoDetalhes.jsx
@@ -3,46 +3,6 @@ import { useParams } from "react-router-dom";
 import NavbarInterna from "./assets/navbarInterna";
 import "./Style/JogoDetalhes.css";
 
-function JogoDetalhes() {
-  const { id } = useParams();
-  const [jogo, setJogo] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("../Json/jogos.json");
-        const jogos = await response.json();
-        const jogoEncontrado = jogos.find((jogo) => jogo.id === id);
-
-        setJogo(jogoEncontrado);
-      }catch(error){
-        console.error("Erro ao carregar os dados:", error);
-      }finally{
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, [id]);
-
-  if(loading){
-    return <div>Carregando informações do jogo...</div>;
-  }
-
-  if (!jogo) {
-    return <div>Jogo não encontrado.</div>;
-  }
-
-  return(
-    <div>
-      <NavbarInterna />
-      <div className="jogo-detalhes-container">
-        <h1 className="jogo-detalhes-titulo">{jogo.nome}</h1>import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import NavbarInterna from "./assets/navbarInterna";
-import "./Style/JogoDetalhes.css";
-
 
 function JogoDetalhes() {
   const{id} = useParams();
@@ -119,25 +79,6 @@ function JogoDetalhes() {
         <button onClick={handleAdicionarAoCarrinho} className="jogo-detalhes-botao">
           Adicionar ao Carrinho
         </button>
-      </div>
-    </div>
-  );
-}
-
-export default JogoDetalhes;
-
-        <img
-          src={jogo.imagem}
-          alt={jogo.nome}
-          className="jogo-detalhes-imagem"
-        />
-        <p className="jogo-detalhes-descricao">{jogo.descricao}</p>
-        <p className="jogo-detalhes-preco">
-          Preço: R$ {(jogo.preco/10).toFixed(2)}
-        </p>
-        <a href="/Carrinho" className="jogo-detalhes-botao">
-          Adicionar ao Carrinho
-        </a>
       </div>
     </div>
   );
