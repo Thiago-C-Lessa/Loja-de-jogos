a27407b369d66bf9d4513acb22a809fad3e16251
Gustavo Aguiar <gustavoaguiarrj2@gmail.com>
2025-01-28 02:02:15 -0300

    HTTPS frontend

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+
+  server:{
+    https:{
+      key: './ssl_Keys/localhost-key.pem',
+      cert:'./ssl_Keys/localhost.pem'
+
+    },
+  },
   plugins: [react()],
 })
