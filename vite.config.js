ae5410202975d7fc7d7974cb89e676f9fcc4dc0e
Caio Leonardo <93164335+Swotty00@users.noreply.github.com>
2025-01-28 02:15:40 -0300

    Att

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import fs from 'fs';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     https: {
-      key: fs.readFileSync('./backend/ssl_Keys/localhost-key.pem'), // Caminho para sua chave privada
-      cert: fs.readFileSync('./backend/ssl_Keys/localhost.pem'),    // Caminho para seu certificado
+      key: fs.readFileSync('./backend/ssl_Keys/localhost-key.pem'),
+      cert: fs.readFileSync('./backend/ssl_Keys/localhost.pem'),
     },
-    host: 'localhost', // Expõe o host local
-    port: 5173,        // Porta padrão do Vite
+    host: 'localhost',
+    port: 5173, 
   },
 });
