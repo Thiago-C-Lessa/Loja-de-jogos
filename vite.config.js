233178a98ff475bd915b916b83a7134f763ec2d1
Caio Leonardo <93164335+Swotty00@users.noreply.github.com>
2025-01-28 02:14:21 -0300

    Correção da Requisição HTTPS

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import fs from 'fs';
 
 // https://vite.dev/config/
 export default defineConfig({
-
-  server:{
-    https:{
-      key: './ssl_Keys/localhost-key.pem',
-      cert:'./ssl_Keys/localhost.pem'
-
-    },
-  },
   plugins: [react()],
-})
+  server: {
+    https: {
+      key: fs.readFileSync('./backend/ssl_Keys/localhost-key.pem'), // Caminho para sua chave privada
+      cert: fs.readFileSync('./backend/ssl_Keys/localhost.pem'),    // Caminho para seu certificado
+    },
+    host: 'localhost', // Expõe o host local
+    port: 5173,        // Porta padrão do Vite
+  },
+});
